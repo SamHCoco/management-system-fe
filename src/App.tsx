84ea3894fa455e5faa3784397a1019804fd57d21
Samuel Coco <Samuel.HenriAC@outlook.com>
2025-11-05 14:01:50 +0000

    Routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import EmployeeGrid from "./components/EmployeeGrid";
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav" bg="white">
-        <NavBar />
-      </GridItem>
-      <GridItem area="aside" bg="gold">
-        Aside
-      </GridItem>
+    <Router>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="white">
+          <NavBar />
+        </GridItem>
 
-      <GridItem area="main" bg="white">
-        <EmployeeGrid />
-      </GridItem>
-    </Grid>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+
+        <GridItem area="main" bg="white">
+          <Routes>
+            {/* TODO - potentially remove */}
+            <Route path="/" element={<Navigate to="/employees" replace />} />
+
+            <Route path="/employees" element={<EmployeeGrid />} />
+
+            <Route path="*" element={<div>Page Not Found</div>} />
+          </Routes>
+        </GridItem>
+      </Grid>
+    </Router>
   );
 }
 
